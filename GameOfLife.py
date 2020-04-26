416481bda60e495964de2f83870a2f3c88f3e8db
aymen_kouah <58881830+aymenkouah@users.noreply.github.com>
2020-04-26 03:53:50 +0200

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,3 +1,17 @@
+#Made by: Kouah Mohammed Aymen
+#Computer science student at "National Computer science Engineering School, Algiers (ESI)"
+#E-mail: jm_kouah@esi.dz
+#Github: https://github.com/aymenkouah
+
+#Requires installaling "pygame"
+#https://www.pygame.org/news
+
+#This is not a game ( a zero player game)
+
+#To understand the rules of this mini society visit
+#https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+
 #Modules and packages
 import pygame
 import time
